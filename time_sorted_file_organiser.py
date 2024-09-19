28968be5c21007dbdcb8497aa165800233460e9a
Re Maak47 <oshaf.khan10@gmail.com>
2024-09-19 14:59:36 +0530

    error handling added

--- time_sorted_file_organiser.py
+++ time_sorted_file_organiser.py
@@ -6,33 +6,52 @@
 import os, sys, re, shutil
 from pathlib import Path
 
+usage = 'Usage: python3 time_sorted_file_organiser.py <path> <prefix> <extension>'
 
 def sorterOrganiser(directory, prefix, suffix):
+    #check whether the directory is present.
+    if not os.path.exists(directory):
+        print(f'Directory: "{directory}" does not exist.')
+        print(usage)
+        sys.exit(1)
+
+    #check if the prefix is applicable.
+    if not re.match(r'^[a-zA-Z_][a-zA-Z0-9_]*$', prefix):
+        print('Invalid prefix. Only letters, numbers and underscores are allowed.')
+        print(usage)
+        sys.exit(1)
+    
+    try:
     # use os.listdir to get all the filenames in a folder.
-    path = Path(directory)
-    files = os.listdir(path)
-    # nameRegex = re.compile(r'^(.*?)\.(.*)$', re.IGNORECASE)
+        path = Path(directory)
+        files = os.listdir(path)
+        # nameRegex = re.compile(r'^(.*?)\.(.*)$', re.IGNORECASE)
 
-    # sort the list according to the modified time using stat().st_mtime.
-    timeStamps = []
-    for file in files:
-        timeStamps.append((Path(directory) / file).stat().st_mtime)
-    print(timeStamps)
-    filesDict = { key:value for key, value in zip(files, timeStamps)}
-    sortedFiles = dict(sorted(filesDict.items(), key=lambda item: item[1]))
-    print(sortedFiles)
+        # sort the list according to the modified time using stat().st_mtime.
+        timeStamps = []
+        for file in files:
+            timeStamps.append((Path(directory) / file).stat().st_mtime)
+        print(timeStamps)
+        filesDict = { key:value for key, value in zip(files, timeStamps)}
+        sortedFiles = dict(sorted(filesDict.items(), key=lambda item: item[1]))
+        print(sortedFiles)
 
-# rename files to have an assigned prefix and a serial number as suffix.
-    serialNum = 1
-    for key in sortedFiles:
-        if suffix in key:
-            shutil.move(Path(path) / key, Path(path) / f'{prefix}{serialNum}.{suffix}')
-            # return the name of the file before and after sorting
-            print(f'{key} is changed to: {prefix}{serialNum}.{suffix}')
-            serialNum += 1
+    # rename files to have an assigned prefix and a serial number as suffix.
+        serialNum = 1
+        for key in sortedFiles:
+            if suffix in key:
+                shutil.move(Path(path) / key, Path(path) / f'{prefix}{serialNum}.{suffix}')
+                # return the name of the file before and after sorting
+                print(f'{key} is changed to: {prefix}{serialNum}.{suffix}')
+                serialNum += 1
+    except FileNotFoundError as e:
+        print(f'File not Found: {e}')
+    except Exception as e:
+        print(f'An error occured: {e}')
 
 if len(sys.argv) != 4:
-    print('Usage: python3 time_sorted_file_organiser.py <path> <prefix> <extension>')
+    print(usage)
     sys.exit(1)
 
-sorterOrganiser(sys.argv[1], sys.argv[2], sys.argv[3])
+if __name__ == "__main__":
+    sorterOrganiser(sys.argv[1], sys.argv[2], sys.argv[3])
